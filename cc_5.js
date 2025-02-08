03cf50281ddd4fa69676a135f609e7012f2857dd
Fernando <mhfernando778@gmail.com>
2025-02-08 13:01:29 -0500

    Task 1 - adding comments

--- cc_5.js
+++ cc_5.js
@@ -1,6 +1,7 @@
 // Task 1 - Customer Profile //
-let customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+let customer = { name: "John Doe", age: 35, email: "john.doe@email.com" }; // declaring an object with customer information
 
-console.log(`Customer Name: ${customer.name}`);
-console.log(`Customer Age: ${customer.age}`);
-console.log(`Customer Email: ${customer.email}`);
+console.log(`Customer Name: ${customer.name}`); // logging the customer name only
+console.log(`Customer Age: ${customer.age}`); // logging the customer age only
+console.log(`Customer Email: ${customer.email}`); // logging the customer email only
+
