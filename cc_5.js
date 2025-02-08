be466a6f1dfbde23725690d6ea8c89a076695fc2
Fernando <mhfernando778@gmail.com>
2025-02-08 13:15:31 -0500

    Task 2 - Order Details done with method

--- cc_5.js
+++ cc_5.js
@@ -5,3 +5,15 @@ console.log(`Customer Name: ${customer.name}`); // logging the customer name onl
 console.log(`Customer Age: ${customer.age}`); // logging the customer age only
 console.log(`Customer Email: ${customer.email}`); // logging the customer email only
 
+
+// Task 2 - Order Details //
+let order = { orderId: 12345, totalAmount: 150, status: "Processing",
+    
+    displayOrder() {
+        console.log(`Order ID: ${this.orderId}`);
+        console.log(`Total Amount: ${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+};
+
+order.displayOrder();
