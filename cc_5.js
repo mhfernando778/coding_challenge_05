2ca002088cd838344a99533786f11078aba09f47
Fernando <mhfernando778@gmail.com>
2025-02-08 12:59:01 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,6 @@
+// Task 1 - Customer Profile //
+let customer = { name: "John Doe", age: 35, email: "john.doe@email.com" };
+
+console.log(`Customer Name: ${customer.name}`);
+console.log(`Customer Age: ${customer.age}`);
+console.log(`Customer Email: ${customer.email}`);
